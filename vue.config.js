032e36362e12869e3ceed344bc9632c25afd887b
pony <1655688657@qq.com>
2020-12-24 19:01:21 +0800

    fix: 添加自动更新

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,10 @@ module.exports = {
       // If you are using Yarn Workspaces, you may have multiple node_modules folders
       // List them all here so that VCP Electron Builder can find them
       nodeModulesPath: ['../../node_modules', './node_modules'],
-      // preload: 'src/preload.js',
-      // // Or, for multiple preload files:
-      // preload: { preload: 'src/preload.js' }
+      // auto update
+      builderOptions: {
+        publish: ['github']
+      }
     }
   }
 }
